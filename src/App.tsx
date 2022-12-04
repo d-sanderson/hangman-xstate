030d49b3eebf0ebcdb6331c26ad63c10930e2458
David <sanderdj90@gmail.com>
2022-12-04 11:51:05 -0700

    even better

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
 
 import './App.css'
 import { useMachine } from '@xstate/react';
-import { useEffect, useState } from 'react';
 import { hangmanMachine } from './machines/hangmanMachine';
-
 
 function App() {
   const [state, send] = useMachine(hangmanMachine);
-  const [retrievedWord, setRetrievedWord] = useState();
-
-  // Load Word 
-  useEffect(() => {
-    const getRandomWord = async () => {
-      const data = await (await fetch('https://random-word-api.herokuapp.com/word')).json()
-      setRetrievedWord(data[0])
-    }
-    getRandomWord()
-  }, [state])
-
-  // Initialize machine to active when word has been retrieved
-  useEffect(() => {
-    if (retrievedWord && state.matches('inactive')) {
-      send('INIT', { word: retrievedWord })
-    }
-  }, [retrievedWord])
-
   const handleInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (/^[a-zA-Z0-9_]{1}$/.test(e.key)) {
       send('MAKEGUESS', { letter: e.key })
--- src/machines/hangmanMachine.ts
+++ src/machines/hangmanMachine.ts
@@ -5,6 +5,7 @@ interface HangmanContext {
   guessedLetters: { correct: string, incorrect: string },
   triesRemaining: number
   hasWon: boolean
+  error: string | null
 }
 
 enum ACTIONS {
@@ -23,7 +24,13 @@ const initialContext: HangmanContext = {
   word: '',
   guessedLetters: { correct: '', incorrect: '' },
   triesRemaining: 7,
-  hasWon: false
+  hasWon: false,
+  error: null,
+}
+
+const fetchRandomWord = async () => {
+  const data = await (await fetch('https://random-word-api.herokuapp.com/word')).json()
+ return data[0]
 }
 
 const handleTries = (context: HangmanContext, event: { letter: string }) => {
@@ -73,7 +80,7 @@ const canSpellWord = (word: string, letters: string) => {
 const handleHasWon = (ctx: HangmanContext) => canSpellWord(ctx.word, ctx.guessedLetters.correct)
 
 export const hangmanMachine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QAsCGA7KBbDBZVAxsgJbpgB0phALsQG5gDEAkgHLMAqA2gAwC6iUAAcA9rGK0R6QSAAeiAKwAWJeQCMADgDsChQE4AbErUAmBWoA0IAJ6INJ8nq0GFBjQadq1OpQF9fVmiYOOj4RKQUNPRMuACCANIAogDiAKqJAMoZvAJIIKLiktJ58ghKJla2CGpu5C5KToZqAMxa3lp6-oEY2HiEJGTkUQyMrADyHABKzJmTiXFsbMk5MgUSxFIypSY8qvpemloaGs08PJY2iCZaDkodmiY7mmd+ASBBvaH9EUMEtCMAdTGkwAImlMhlEiCVnk1kUtldduR9l5tMdTudKohmrU9GpjGo9M0TK0Ok4uu8eiEwgNIn9oowYcIxOtNiVEXs8aijiczhcqsSHHpyjpmsYFDcFBSPtTvoNhkwuGpcszChtiqBtkiUYd0XysQhHgZkTwjjUTEStEpdBppVS+uFBgB3UiMOaQ7j8VYs+HshAKVrkHjNPQ8UPGDS6O4G04KcghhTnJxaVo7W1vGUO2nkAA2YiY7sSnpV+R96oR-sDwdD4c0Ua0BrTjgMZ3sLm8xma-je6BEEDgMkzX0dYG9arZmsQAFoDAaZ0GzudlKbms1IyS7cEsz8qPSGGPWRq5IgDC5yDozuZ9GuJc0Daig14eCcLYn3PpN58aT8FQffZP-Q0ZFdCcJRmjUMM9BMdxZ0uQ0PHPDsrQ0HgPBTdNui3YdsxdI84XLP1jC0YC9CgngzEeJRgxja5HGJLQrRxSN9E6DN7Wwn481gUdYTLCdjzKbwSLIiiTCou84JJNRyAaI5UyUFwUNeTCvzlCgwHQVAACMc0gP8CIAhQgN0fRGIg0joKUGi9HPXYFOfCDdBqbtfCAA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QAsCGA7KBbDBZVAxsgJbpgB0phALsQG5gDEEA9mZenSwNYUzUAlDKywB1FgCcIAbQAMAXUSgADi1jFabJSAAeiAKwBmAOzlZhgJwBGAGzmbxgEz6ALDYA0IAJ6IAHI-ILYxt9O2MXKwsbBwBfGM80TBx0fCJSCioCWgZGMAkJSXJlABtUagAzSSxyfiF0EXEpOUUkEFV1TXRtPQQLfQtyS31fKONZWV9DV08fBEjZQYcrcNCXcasXOISMbDxCEnYaeiZcAEEAaQBRAHEAVUuAZQfm7XaNYi1WnpdHGcRbAZrQw2RwmCyyKyGYH6LYgRK7FL7dLkI45AByAHkACoCACSjwElzOuLRJOuL1ab063UQjlkLnI-SsG3GLmC1lkHm8tOMhnIjkcxkm-hc+lk+iFsPhyVSBwoqKYogxAgAIndHg9LiqKSo1O9PqAenSGUyWfT2VZOX8EMCBtFBTZLKKooYXIYpTsZUjDlljowdW09dSvrT6YzrGa2TYOVzZoZQeRo5DfKExoKNh6kns0j7skxpFYWrqOh8uiGEMbw8y1uak1buRXBfzQmLosZIu3HJt4nDPdm5eQAO6kRiEzVYgNU0s0hBGUzmax2YFOVyxxCGTnkDbAn6ggXR3yZhGy5HFNRMMeXCcKV5B6fludmSy2ewrtzWukDMa+YwSrs2UUQSPL0cwocpUGIYoAFcJAvR4r0nO8DV0f4JgCExgl8S12wsSZrQsBkNzccFDEmekISsOIe3QFgIDgbRpX7dJbxLZCegAWjXBBOLMcYIWMYJPxsXxzGApj2EyPMWP1MtDUQYxwUGJwo0haN6V8a1sLMKx-EcCxHAceN0zExFQJRX0GGk4M5NnXxGX0foRIM9TZAUlwPzGcg3BsbCgktAighMk92GHWTA1YsLvmWeyLAsDdXFcZkuPjOzhJZVxJl5YIYR7RjTIHM9YDAKz7xsiJTAc2L4tFCJbA-eN+TcfQdLGWxowsILvTAiDoNgkq2P+OkrDMXwUzGkEBMiTTnC3XklzbFxwWGKiYiAA */
   createMachine<HangmanContext, any>({
   context: initialContext,
   schema: { context: {} as HangmanContext, events: {} as any },
@@ -82,11 +89,21 @@ export const hangmanMachine =
   initial: "inactive",
   states: {
     inactive: {
-      on: {
-        INIT: {
-          target: "active",
-          actions: ACTIONS.SETWORD,
-        },
+      invoke: {
+        src: () => fetchRandomWord(),
+        id: "getRandomWord",
+        onDone: [
+          {
+            target: "active",
+            actions: assign({ word: (ctx, event) => event.data }),
+          },
+        ],
+        onError: [
+          {
+            target: "failure",
+            actions: assign({ error: (context, event) => event.data }),
+          },
+        ],
       },
     },
     active: {
@@ -130,7 +147,14 @@ export const hangmanMachine =
         },
       },
     },
-    enabled: {},
+    failure: {
+      on: {
+        RESET: {
+          target: "inactive",
+          actions: "RESET",
+        },
+      },
+    },
   },
 }, {
   guards: {
