58a2da5918ad8c37cbf6cc52eb6177fb19e8bb2e
David <sanderdj90@gmail.com>
2022-12-04 13:57:33 -0700

    woot inspector!

--- src/components/HangmanUI.tsx
+++ src/components/HangmanUI.tsx
@@ -4,8 +4,8 @@ import { hangmanMachine } from '../machines/hangmanMachine';
 import { ACTIONS, STATES } from '../machines/hangmanMachine.types';
 
 const HangmanUI = () => {
-  const [state, send] = useMachine(hangmanMachine);
-
+  const [state, send] = useMachine(hangmanMachine, { devTools: true })
+  const active = state.matches('active')
   const handleInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (/^[a-zA-Z0-9_]{1}$/.test(e.key)) {
       send('MAKEGUESS', { letter: e.key })
@@ -16,7 +16,7 @@ const HangmanUI = () => {
     <div>
       <h1>{state.toStrings()}</h1>
       <pre>{JSON.stringify(state.context, null, 2)}</pre>
-      {state.matches('active') &&
+      {active &&
         <input
           onKeyDown={(e) => handleInput(e)}
         />
