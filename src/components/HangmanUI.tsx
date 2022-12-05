2cc035e69e4925180e21571f22f2bcdafbffa47e
David <sanderdj90@gmail.com>
2022-12-05 00:25:20 -0700

    lastbit

--- src/components/HangmanUI.tsx
+++ src/components/HangmanUI.tsx
@@ -9,7 +9,7 @@ const HangmanUI = () => {
   const [state, send, service] = useMachine(hangmanMachine, { devTools: true })
   const active = state.matches('active')
   const handleInput = (e: React.KeyboardEvent<HTMLInputElement>) => send('MAKEGUESS', { letter: e.key })
-
+  console.log(JSON.stringify(state.context, null, 2))
   return (
     <div>
       <SplineScene state={state} service={service} />
@@ -19,12 +19,16 @@ const HangmanUI = () => {
         />
       }
       <h1>{state.toStrings()}</h1>
+      <h1>{state.context.word
+        .split('')
+        .map(el => state.context.guessedLetters.correct.includes(el) ? ` ${el} ` : ' _ ')
+        .join('')}
+      </h1>
       {state.can(ACTIONS.RESET) &&
         <button onClick={() => {
           send(ACTIONS.RESET)
         }}>reset</button>
       }
-      <pre>{JSON.stringify(state.context, null, 2)}</pre>
     </div>
   )
 }
